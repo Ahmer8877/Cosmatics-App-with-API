0d23775e7da71779f4f4969ea354377b4c7498a2
Ahmer8877 <rajpootahmer786@gmail.com>
2025-10-01 20:20:50 +0500

    My own notification

--- app/src/main/java/com/e/ahmer/apisuportedapp/MainActivity.kt
+++ app/src/main/java/com/e/ahmer/apisuportedapp/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.e.ahmer.apisuportedapp
 
 import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
@@ -12,8 +21,14 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+@Suppress("CAST_NEVER_SUCCEEDS", "LocalVariableName")
 @SuppressLint("StaticFieldLeak")
 class MainActivity : AppCompatActivity() {
+
+    val channelId="channel id"
+    val channelName="channel name"
+    val notificationId=0
+    @SuppressLint("MissingPermission")
 
     //late defind variable for myrecycleview
     lateinit var myrecycleview : RecyclerView
@@ -21,9 +36,41 @@ class MainActivity : AppCompatActivity() {
     lateinit var myAdapter: MyAdapter
 
 
+
+    @SuppressLint("MissingPermission")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //defind the MyNotificationChannel func.
+        MyNotificationChannel()
+
+        //pending intent code
+        val intent= Intent(this, MainActivity::class.java)
+        val pendingIntent= PendingIntent.getActivity(this,0,intent, PendingIntent.FLAG_MUTABLE)
+
+
+        //set the notification display
+        val Notification= NotificationCompat.Builder(this,channelId)
+            .setContentTitle("My Notification")
+            .setContentText("I check the andiod notification Systems")
+            .setSmallIcon(R.drawable.baseline_message_24)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        //manage the create notification
+        val notificationManager= NotificationManagerCompat.from(this)
+
+        //notify the my own notification
+        val notificationBtn=findViewById<ImageButton>(R.id.imageButton)
+
+        notificationBtn.setOnClickListener {
+            notificationManager.notify(notificationId,Notification)
+        }
+
 
         //myrecycleview is defind with id
         myrecycleview=findViewById(R.id.myrecycleview)
@@ -61,5 +108,23 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+    }
+
+    // MyNotificationChannel func. for oreo version android
+    @Suppress("FunctionName")
+    @SuppressLint("ObsoleteSdkInt")
+    private fun MyNotificationChannel(){
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+            val channels= NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description="This is my Notification"
+                lightColor= Color.GREEN
+                enableLights(true)
+            }
+            val manager= getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channels)
+
+        }
     }
 }
